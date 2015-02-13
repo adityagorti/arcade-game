75ed887b06f2b39801057e7799f13e2daec82e5c
Aditya <adityagorti6@gmail.com>
2015-02-12 20:19:50 -0800

    first attempt, working on setting up player class

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x,y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
+
 }
 
 // Update the enemy's position, required method for game
@@ -14,6 +17,8 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x = this.x + this.x * dt;
+    if(this.x > 606){this.x = 0;}
 }
 
 // Draw the enemy on the screen, required method for game
@@ -24,12 +29,22 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-
+var Player = function(){
+    this.sprite = 'images/char-boy.png';
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+var allEnemies = [];
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+allEnemies[0]= new Enemy(getRandomArbitrary(0,500),160);
+allEnemies[1] = new Enemy(getRandomArbitrary(0,500),240);
+allEnemies[3] = new Enemy(getRandomArbitrary(0,500), 320);
 
 
 // This listens for key presses and sends the keys to your
