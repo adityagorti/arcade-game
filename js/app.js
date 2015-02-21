f5e73f590b09af7e3c44fd68477ddd114c682209
Aditya <adityagorti6@gmail.com>
2015-02-20 18:21:52 -0800

    working on collision detection and setting enemy speed

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+function randomYPos(){
+     return Math.round(getRandomArbitrary(1,3))*75;
+}
 // Enemies our player must avoid
 var Enemy = function(x,y) {
     // Variables applied to each of our instances go here,
@@ -8,7 +15,6 @@ var Enemy = function(x,y) {
     this.sprite = 'images/enemy-bug.png';
     this.x = x;
     this.y = y;
-
 }
 
 // Update the enemy's position, required method for game
@@ -17,8 +23,15 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.x = this.x + this.x * dt;
-    if(this.x > 606){this.x = 0;}
+    this.x = this.x+this.x *dt;
+    if(this.x>606){
+        this.x=this.x+this.x*dt-606;
+        this.y=randomYPos();
+    }
+    if (this.x===player.x || this.y===player.y){
+        player.x=202;
+        player.y=415;
+    }
 }
 
 // Draw the enemy on the screen, required method for game
@@ -37,36 +50,41 @@ var Player = function(x,y){
 }
 
 Player.prototype.update = function() {
-    this.x = 
+    this.x=this.x;
+    this.y=this.y;
+
+
+
+}
+
+Player.prototype.render = function(){
+    ctx.drawImage(Resources.get(this.sprite),this.x,this.y);
 }
 
 Player.prototype.handleInput = function(keys){
     switch(keys){
-        case 'left' : this.x-83;
+        case 'left' : this.x=this.x-83;
                         break;
-        case 'right': this.x+83;
+        case 'right': this.x=this.x+83;
                         break;
-        case 'up': this.y+101;
+        case 'up': this.y=this.y-75;
                         break;
-        case 'down': this.y-101;
+        case 'down': this.y=this.y+75;
                         break;
             }
 }
 
-var player = new Player(303, 415);
+var player = new Player(202, 415);
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 var allEnemies = [];
-function getRandomArbitrary(min, max) {
-    return Math.random() * (max - min) + min;
-}
+
 
 allEnemies[0]= new Enemy(getRandomArbitrary(0,500),60);
 allEnemies[1] = new Enemy(getRandomArbitrary(0,500),154);
 allEnemies[2] = new Enemy(getRandomArbitrary(0,500), 234);
 
-//player
 
 
 
